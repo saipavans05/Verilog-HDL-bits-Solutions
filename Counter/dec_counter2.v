7d44b8dd520515d220a84af19fa67f501e28c10f
S Sai Pavan <saipavans8341@gmail.com>
2026-03-10 12:55:39 +0530

    Create dec_counter2.v

---
+++ Counter/dec_counter2.v
@@ -0,0 +1,13 @@
+module top_module (
+    input clk,
+    input reset,
+    output [3:0] q);
+    always @ (posedge clk) begin
+        if(reset)
+            q <= 4'b1 ;
+        else if (q == 4'd10)
+            q <= 4'b1 ;
+        else 
+            q <= q + 4'b1 ;
+    end
+endmodule
