623dfa0601f9b4af7edbea9d1f62e022af4d1a5a
S Sai Pavan <saipavans8341@gmail.com>
2026-03-10 12:46:06 +0530

    Update priority_encoder_casez.v

--- Procedures/priority_encoder_casez.v
+++ Procedures/priority_encoder_casez.v
@@ -1,17 +1,18 @@
-// synthesis verilog_input_version verilog_2001
+ // synthesis verilog_input_version verilog_2001
 module top_module (
-    input [3:0] in,
-    output reg [1:0] pos  );
+    input [7:0] in,
+    output reg [2:0] pos );
     always @ (*) begin
-        if(in[0])
-            pos = 2'b00 ;
-        else if (in[1])
-            pos = 2'b01 ;
-        else if (in[2])
-            pos = 2'b10 ;
-        else if (in[3])
-            pos = 2'b11 ;
-        else
-            pos = 0 ;
+        casez(in[7:0])
+             8'bzzzzzzz1 : pos = 3'b000 ;
+             8'bzzzzzz1z : pos = 3'b001 ;
+             8'bzzzzz1zz : pos = 3'b010 ;
+             8'bzzzz1zzz : pos = 3'b011 ;
+             8'bzzz1zzzz : pos = 3'b100 ;
+             8'bzz1zzzzz : pos = 3'b101 ;
+             8'bz1zzzzzz : pos = 3'b110 ;
+             8'b1zzzzzzz : pos = 3'b111 ;
+            default : pos = 0 ;
+        endcase
     end
 endmodule
